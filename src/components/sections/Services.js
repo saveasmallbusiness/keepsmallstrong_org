6557b6d1ac2dcca1df4f7b62646e0a3372de71c7
Adam Grohs <adam@uniquelyparticular.com>
2020-05-17 16:04:57 -0500

    chore: added interim anchor link position

--- src/components/sections/Services.js
+++ src/components/sections/Services.js
@@ -9,8 +9,8 @@ const Services = () => (
     className="pb-12 lg:pb-24"
   >
     <div className="container mx-auto pt-12 lg:pt-24 md:bg-right-top md:bg-no-repeat bg-auto md:bg-contain services-bg">
-      <div className="md:flex md:items-center md:justify-start pb-12 md:pb-40">
-        <div className="md:w-1/3 text-left mx-6 md:mx-12">
+      <div className="md:flex md:items-center md:justify-start">
+        <div className="md:w-1/3 text-left mx-6 md:mx-12 pb-2 md:pb-8">
           <h3 className="font-heading font-bold text-2xl md:text-4xl uppercase text-secondary leading-tight pb-4 md:pb-8">
             Big Agency Help
             <br />
@@ -21,6 +21,7 @@ const Services = () => (
           </p>
         </div>
       </div>
+      <div id="get-help" className="pb-12 md:pb-24"/>
       <div className="lg:flex lg:items-center lg:justify-start pb-12 lg:pb-24">
         <div className="lg:w-1/3 text-left mx-6 md:mx-12">
           <h3 className="font-heading font-bold text-2xl md:text-4xl uppercase leading-tight pb-4">E-Commerce</h3>
@@ -36,17 +37,24 @@ const Services = () => (
             We can assist with setting up delivery, ship-from-store as well as logistics and customer service.
           </p>
         </div>
-        <div className="lg:w-2/3 text-center mx-6 pt-24 lg:pt-0 md:mx-12 lg:py-6">
+        <div className="lg:w-2/3 text-center mx-6 pt-24 lg:pt-24 md:mx-24">
+          <p className="pb-6 px-24 lg:pt-40">
+            Whether you're the business owner or just a fan, let us know and we're happy to help.
+          </p>
           <Button
             size="lg"
-            className="bg-privary rounded-full"
+            className="bg-privary m-2 rounded-full"
+            onClick={() => navigate('/apply')}
+          >
+            <span className="text-md">submit your business</span>
+          </Button>
+          <Button
+            size="lg"
+            className="bg-privary m-2 rounded-full"
             onClick={() => navigate('/refer')}
           >
-            <span className="text-md">submit a business</span>
+            <span className="text-md">refer a business</span>
           </Button>
-          <p className="pt-4 md:pt-12 md:px-24">
-            Whether you're the business owner or just a fan, let us know if we can help.
-          </p>
         </div>
       </div>
     </div>
