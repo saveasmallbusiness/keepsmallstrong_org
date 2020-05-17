a0471ca6b23ea3e1c38a8a82d8e1b5ad0385ce1e
Adam Grohs <adam@uniquelyparticular.com>
2020-05-17 15:59:37 -0500

    chore: added bg-grey for article quote band

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,6 +62,7 @@ module.exports = {
           'bg-secondary',
           'bg-accent',
           'bg-dark',
+          'bg-grey',
           'carousel',
           'services-bg',
           'is-sticky',
