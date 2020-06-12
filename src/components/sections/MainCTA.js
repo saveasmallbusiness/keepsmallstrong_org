13ea15acb9414b12b681ca017ce2f642800bf3b9
Adam Grohs <adam@uniquelyparticular.com>
2020-06-12 08:12:26 -0500

    chore: minor changes to copy

--- src/components/sections/MainCTA.js
+++ src/components/sections/MainCTA.js
@@ -6,10 +6,10 @@ import Button from '../Button';
 const MainCTA = () => (
   <div className="px-8 md:px-16 pb-12 md:pb-24">
     <div className="flex flex-col lg:flex-row font-headline font-bold uppercase text-xl lg:text-3xl pb-4 leading-tight justify-center text-center">
-      A collective effort to save small business in America.
+      A collective effort to help small business in America
     </div>
     <div className="flex flex-col lg:flex-row mb-8 lg:text-xl leading-tight justify-center text-center">
-      offering online tools to grow digital business and a marketplace to buy direct.
+      offering online tools to grow digital business and a providing marketplace to buy direct.
     </div>
     <div className="block md:flex flex-col md:flex-row">
       <div className="md:flex-1 mx-2 text-center md:text-right mb-4 md:mb-0">
